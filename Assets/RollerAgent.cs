9c8e92a29af412b15c6760d652df8bcfd2254548
Naevyys <siriremerk@gmail.com>
2022-10-04 19:05:55 +0200

    Solved issues with training #2 #3 #6

--- Assets/RollerAgent.cs
+++ Assets/RollerAgent.cs
@@ -6,53 +6,44 @@ using MLAgents;
 public class RollerAgent : Agent
 {
     Rigidbody rBody;
+    public Transform Target;
+    private float speed = 1f;
+    private float rotationScalingFactor = 90f;  //f means float
+
     void Start () {
         rBody = GetComponent<Rigidbody>();
     }
 
-    public Transform Target;
     public override void AgentReset()
     {
-        if (this.transform.position.y < 0)
-        {
-            // If the Agent fell, zero its momentum
-            this.rBody.angularVelocity = Vector3.zero;
-            this.rBody.velocity = Vector3.zero;
-            this.transform.position = new Vector3( 0, 0.5f, 0);
-            // Rotate agent randomly
-            this.transform.rotation = Quaternion.Euler(Vector3.up * Random.Range(0f, 360f));
-        }
+        // Zero momentum
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
 
         // Move the target to a new spot
         Target.position = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+        // Move agent back to center
+        this.transform.position = new Vector3( 0, 0.5f, 0);
+        // Rotate agent randomly
+        this.transform.rotation = Quaternion.Euler(Vector3.up * Random.Range(0f, 360f));
     }
 
     public override void CollectObservations()
     {
-        // Target and Agent positions
-        // AddVectorObs(Target.position);  // We don't want the agent to know the target position anymore
-        // AddVectorObs(this.transform.position);
 
-        // Agent rotation
-        // AddVectorObs(this.transform.rotation);
-
-        // Agent velocity
-        AddVectorObs(rBody.velocity.x);
-        AddVectorObs(rBody.velocity.z);
     }
 
-    public float speed = 10;
     public override void AgentAction(float[] vectorAction)
     {
         // Actions, size = 2 (first is for movement, second for rotation)
 
         // Move
-        Vector3 controlSignalMovement = this.transform.forward * vectorAction[0]; // Move forward only
-        rBody.AddForce(controlSignalMovement * speed);
+        Vector3 controlSignalMovement = (Vector3.forward * Mathf.Abs(vectorAction[0]));
+        rBody.transform.Translate(controlSignalMovement * speed * Time.deltaTime);  // Translate is relative to Space.Self by default, so need to use Vector3.forward...
 
         // Turn
-        Vector3 controlSignalRotation = this.transform.up * vectorAction[1];
-        rBody.AddTorque(controlSignalRotation);
+        Vector3 controlSignalRotation = Vector3.up * vectorAction[1];
+        rBody.transform.Rotate(controlSignalRotation * rotationScalingFactor * Time.deltaTime);
 
         // Rewards
         float distanceToTarget = Vector3.Distance(this.transform.position, Target.position);
@@ -71,13 +62,15 @@ public class RollerAgent : Agent
             Done();
         }
 
+        // If not done, add small negative reward to encourage the agent to solve the task as fast as possible
+        SetReward(-0.01f);
     }
 
     public override float[] Heuristic()
     {
         var action = new float[2];
-        action[0] = Input.GetAxis("Horizontal");
-        action[1] = Input.GetAxis("Vertical");
+        action[1] = Input.GetAxis("Horizontal");
+        action[0] = Input.GetAxis("Vertical");
         return action;
     }
 
--- Assets/ViewAgentTracker.cs
+++ Assets/ViewAgentTracker.cs
@@ -6,19 +6,17 @@ public class ViewAgentTracker : MonoBehaviour
 {
 
     public GameObject player;
-    //private Vector3 offset;
-    //private Vector3 rotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        //offset = this.transform.position - player.transform.position;
+
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        this.transform.position = player.transform.position;// + offset;
+        this.transform.position = player.transform.position;
         this.transform.rotation = Quaternion.Euler(player.transform.rotation.eulerAngles.y * Vector3.up);
     }
 }
