c0df3ad58f95c9643da4daa9e2e8a7dc34cf3e9f
Naevyys <siriremerk@gmail.com>
2022-10-10 16:54:53 +0200

    Starting to implement access with python API #8, not done yet, problems with renaming across mlagents versions

---
+++ Assets/Python_scripts/train_RollerAgent.py
@@ -0,0 +1,6 @@
+import mlagents
+from cnn import VisualQNetwork
+from mlagents_envs.registry import default_registry
+from mlagents_envs.environment import UnityEnvironment
+import matplotlib.pyplot as plt
+
