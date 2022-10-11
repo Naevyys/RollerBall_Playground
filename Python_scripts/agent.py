07b0e33782d6c1c13c074f920a7972cbf34b982b
Naevyys <siriremerk@gmail.com>
2022-10-11 18:07:10 +0200

    Code cleaning before next step + added notebook for easier code execution

--- Python_scripts/agent.py
+++ Python_scripts/agent.py
@@ -2,25 +2,38 @@ import numpy as np
 
 
 # Implement CNN here or in a new file
+# Use softmax or sigmoid? Softmax might be more appropriate if we want the agent to either turn or move forward, while sigmoid is better if we want the agent to do both
+# Check range of predictions needed to turn in both directions & move only forward
+#   Note: Moving only forward is currently enforced in RollerAgent.cs, but it would be cool to enforce it in the predictions of the CNN directly instead, then we can also
+#   change that later in case we want the agent to be able to move backwards too
 
 
 class Agent:
     def __init__(self, num: int, group, group_specs, vision_decoder = None) -> None:  # TODO: change vision_decoder
+        '''
+        Initialize the agent with identifiers and its vision decoder.
+        '''
         self.index = num  # This one will not be incremented
         self.num = num
         self.group = group
         self.group_specs = group_specs
         self.vision_decoder = vision_decoder
     
-    def get_action(self, observations):
+    def get_action(self, observations, action_size=2):
+        '''
+        Compute the actions from the observations using the vision decoder of the agent.
+        '''
         # Pass observations through cnn
         # return actions
-        return np.random.rand(2).reshape((1,-1))
+        return np.random.rand(action_size).reshape((1,-1))
     
     def update_encoder(self):
         # Do a gradient step to update the cnn
         raise NotImplementedError
     
     def increment_agent_num(self, amount = 1):
+        '''
+        To keep up with the incrementation of agent ids after environment reset.
+        '''
         assert self.num + amount >= 0, "Number of the agent should not be negative."
         self.num += amount
--- Python_scripts/trainer.py
+++ Python_scripts/trainer.py
@@ -16,12 +16,19 @@ class Trainer:
             self.agent_groups.append(Agent(0, group, specs))
 
     def reset_environment(self):
+        '''
+        Reset env after terminal state reached.
+        '''
         self.env.reset()
         for agent in self.agent_groups:  # Increment all agents
             agent.increment_agent_num()  # If several agent groups, might need to increment by another amount than the default 1. There is an optional argument to change the amount
 
     def loop(self):
-        # Get actions
+        '''
+        - Get observations and corresponding actions for each agent
+        - Step the environment
+        - Check for terminal state, reset env if terminal state reached
+        '''
         for agent in self.agent_groups:  # I should only have one
             batch_result = self.env.get_step_result(agent.group)
             step_result = batch_result.get_agent_step_result(agent.num)  # When agent gets reset, its ID changes...
@@ -46,29 +53,16 @@ class Trainer:
             self.terminal = False
     
     def get_agents(self):
+        '''
+        Returns the agent groups list
+        '''
         return self.agent_groups
     
+    def close_env(self):
+        self.env.close()
+
     def run_steps(self, num_steps = 10):
+        # Just for testing, will be removed afterwards
         for _ in range(num_steps):
             self.loop()
-
-
-env_file = b"C:\Users\sirir\Desktop\University\Master\Semester_3\Data_Science_Semester_Project\Unity_projects\RollerBall_Playground\Builds\RollerBall_Playground.exe".decode("utf-8")
-
-trainer = Trainer(env_file)
-trainer.run_steps(100)
-
-# Side channels are communication paths unseen by the agent
-# Otherwise can just pass information through observations but not use them to obtain an action
-
-
-# Build a loop:
-# - Collect step results from my agent
-# - Feeds visual observation to CNN
-# - Get action prediction from CNN
-# - Sets them as actions
-# - Steps the environment
-
-
-# Start by having random actions to test the loop
-# Then use CNN
+        self.close_env()
